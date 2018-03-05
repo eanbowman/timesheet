8eb52c11ad3afd00d2336bbe87229fcda160f589
eanbowman@gmail.com <eanbowman@gmail.com>
2018-03-05 16:33:50 -0500

    [Styles] Minor timesheet style update

--- views/template/timesheet_listing.php
+++ views/template/timesheet_listing.php
@@ -15,17 +15,17 @@ foreach( $timesheets as $key => $item ) {
 	if( $count > 20 ) break;
 ?>
 	<li class="timesheet">
-		<div class="timesheet-id"><?php echo $item->id; ?></div>
-		<div class="timesheet-docketID"><?php echo $item->docketID; ?></div>
-		<div class="timesheet-date"><?php echo $item->date; ?></div>
-		<div class="timesheet-start"><?php echo $item->start; ?></div>
-		<div class="timesheet-end"><?php echo $item->end; ?></div>
-		<div class="timesheet-empID"><?php echo $item->empID; ?></div>
-		<div class="timesheet-description"><?php echo $item->description; ?></div>
-		<div class="timesheet-task"><?php echo $item->task; ?></div>
-		<div class="timesheet-lastModified"><?php echo $item->lastModified; ?></div>
-		<div class="timesheet-subtask"><?php echo $item->subtask; ?></div>
-		<div class="timesheet-isAClientChange"><?php echo $item->isAClientChange; ?></div>
+		<div class="timesheet-id"><label>ID:</label> <?php echo $item->id; ?></div>
+		<div class="timesheet-docketID"><label>Docket:</label> <?php echo $item->docketID; ?></div>
+		<div class="timesheet-date"><label>Date:</label> <?php echo $item->date; ?></div>
+		<div class="timesheet-start"><label>Started:</label> <?php echo $item->start; ?></div>
+		<div class="timesheet-end"><label>Finished:</label> <?php echo $item->end; ?></div>
+		<div class="timesheet-empID"><label>Employee</label> <?php echo $item->empID; ?></div>
+		<div class="timesheet-description"><label>Description</label><br><?php echo $item->description; ?></div>
+		<div class="timesheet-task"><label>Task:</label> <?php echo $item->task; ?></div>
+		<!-- div class="timesheet-lastModified"><label>Modified:</label> <?php echo $item->lastModified; ?></div -->
+		<div class="timesheet-subtask"><label>Subtask:</label> <?php echo $item->subtask; ?></div>
+		<div class="timesheet-isAClientChange"><label>Change:</label> <?php echo $item->isAClientChange; ?></div>
 		<div class="timesheet-actions">
 			<a href="?show=timesheet&timesheet=<?php echo $item->id; ?>&action=edit" class="timesheet-action-edit">Edit</a>
 			<a href="?show=timesheet&timesheet=<?php echo $item->id; ?>&action=delete" class="timesheet-action-delete" onclick="confirm('Are you sure you want to delete this permanently?');">Delete</a>
@@ -35,25 +35,4 @@ foreach( $timesheets as $key => $item ) {
 }
 ?>
 </ul>
-
-<style type="text/css">
-.timesheet-listing {
-	background: whitesmoke;
-	margin: 2%;
-	padding: 0;
-	width: 96%;
-}
-.timesheet-listing .timesheet {
-	display: table-row;
-	list-style: none;
-	padding: 10px;
-}
-.timesheet-listing .timesheet:nth-child(even) {
-	background: white;
-}
-.timesheet-listing .timesheet > div {
-	display: table-cell;
-}
-</style>
-
 
