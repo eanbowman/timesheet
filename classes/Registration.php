7b01bca38c94e191cd21795e61bc2f0049b9e254
Ean Bowman <eanbowman@gmail.com>
2018-09-28 12:26:35 -0400

    utf8mb4

--- classes/Registration.php
+++ classes/Registration.php
@@ -69,7 +69,7 @@ class Registration
             $this->db_connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
             // change character set to utf8 and check it
-            if (!$this->db_connection->set_charset("utf8")) {
+            if (!$this->db_connection->set_charset("utf8mb4")) {
                 $this->errors[] = $this->db_connection->error;
             }
 
